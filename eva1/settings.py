4e92a2534d9c1e202972a5c3226ec685d7854ff2
ElvisCuro <elviscuro0@gmail.com>
2024-10-29 23:35:31 -0500

    update SQP

--- eva1/settings.py
+++ eva1/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+
 import os
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +47,7 @@ INSTALLED_APPS = [
     
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://template-django-production-6e65.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://spectrophotometer-production.up.railway.app']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -80,11 +84,17 @@ WSGI_APPLICATION = 'eva1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:zItFJotjGpTxvxYDsuXkXnBSfzVfQsAs@autorack.proxy.rlwy.net:28419/railway'
+    )
 }
 
 # mysql://root:eoAyMxFrtgLwxkyanqYPNJuwpVDISamS@autorack.proxy.rlwy.net:11502/railway
